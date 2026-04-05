b5f6b70c2e3fc47e92ad7370d5a1ae8697571f60
selimayann <ayancristian559@gmail.com>
2026-04-05 21:22:59 +0300

    sharpe ratio in task1.c citire preturi din n.txt adaug valori in lista folosind addAtEnd calculul sharpe ratio output in out.txt

---
+++ task1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "list.h"
+
+int main() {
+    FILE *in = fopen("in.txt", "r");
+    FILE *out = fopen("out.txt", "w");
+    int n;
+    double p;
+
+    fscanf(in, "%d", &n);
+
+    Node* head = NULL;
+
+    for (int i = 0; i < n ; i++) {
+        fscanf (in, "%lf", &p);
+        addAtEnd(&head, p);
+    }
+
+    if (head !=NULL) print(head);
+
+    if (head == NULL || head -> next ==NULL) {
+        fprintf(out, "nu se poate calcula");
+        fclose(out);
+        fclose(in);
+        return 0;
+    }
+
+    double suma1 = 0, suma2 = 0, mediaR, sigma, sRatio;
+
+    const Node* np = head;
+    const Node* nc = head -> next;
+
+    while  (nc != NULL) {
+    double Rt = (nc -> price - np -> price) / np -> price;
+    suma1 = suma1 + Rt;
+    suma2 = suma2 + (Rt * Rt);
+
+    np = nc;
+    nc = nc -> next;
+    }
+    mediaR = suma1 / (n-1);
+    sigma = sqrt(suma2 / (n-1) - (mediaR * mediaR));
+    sRatio = mediaR / sigma;
+
+    fprintf(out, "randamentul mediu = %lf \n", mediaR);
+    fprintf(out, "volatilitatea = %lf \n", sigma);
+    fprintf(out, "sharpe ratio = %lf \n", sRatio);
+    
+    fclose(out);
+    fclose(in);
+}
